ebc92184d654a57a3a4f8ee155c43cecfbce0d5e
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-09 20:07:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,11 +35,11 @@ function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
 for(let i =0; i<encodedStr.length;i++){
-	if(lookup.encodedStr[i] === undefined){
-		decodedArr.push(encodedStr[i])
+	if(if encodedStr[i] in lookup){
+		decodedArr.push(lookup.encodedStr[i])	
 	}
 	else{
-		decodedArr.push(lookup.encodedStr[i])
+		decodedArr.push(encodedStr[i])
 	}
 }
 
