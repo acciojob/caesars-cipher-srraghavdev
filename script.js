7864f1131415bfcf064a657e2a4a0f92737643b6
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-09 19:59:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ function rot13(encodedStr) {
   // Only change code below this line
 for(let i =0; i<encodedStr.length;i++){
 	if(lookup.encodedStr[i] === undefined){
-		decodedArr[]+=(encodedStr[i])
+		decodedArr.push(encodedStr[i])
 	}
 	else{
-		decodedArr+=(lookup.encodedStr[i])
+		decodedArr.push(lookup.encodedStr[i])
 	}
 }
 
-  return; //return decodedArr
+  return decodedArr.join(""); //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
